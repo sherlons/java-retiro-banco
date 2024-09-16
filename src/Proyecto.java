5e870c0c81f21acbd6e1668180d507e534210794
sherlons <lukasteoo03@gmail.com>
2024-09-16 15:31:19 -0500

    datos del usuario

---
+++ src/Proyecto.java
@@ -0,0 +1,13 @@
+public class Proyecto {
+    public static void main(String[] args) {
+        System.out.println("\n*******Bienbenido al banco Shelons********");
+        String nombre = "shakira";
+        String tipoDeCuenta = "corriente";
+        double saldo = 5000.500;
+        System.out.println ("********************");
+        System.out.println("\nNombre de cliente" + nombre);
+        System.out.println("Tipo de Cuenta" + tipoDeCuenta);
+        System.out.println("saldo disponible" + saldo + "$");
+        System.out.println("**********************");
+    }
+}
